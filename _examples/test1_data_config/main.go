38b710882dade90c84f8493cf7190575ee76af5b
ryansu <phantacix@qq.com>
2021-03-03 18:41:06 +0800

    clean code

--- _examples/test1_data_config/main.go
+++ _examples/test1_data_config/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/cherry-game/cherry"
 	"github.com/cherry-game/cherry/_examples/test1_handler/mocks"
-	cherryConst "github.com/cherry-game/cherry/const"
+	"github.com/cherry-game/cherry/const"
 	"github.com/cherry-game/cherry/data_config"
 	"github.com/cherry-game/cherry/handler"
 	"github.com/cherry-game/cherry/logger"
