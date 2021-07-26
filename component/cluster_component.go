f4d77885f2ec1ae31dd8f998288018c417c1b0fd
ryansu <phantacix@qq.com>
2021-07-26 12:08:23 +0800

    clean code

--- component/cluster_component.go
+++
@@ -1,9 +0,0 @@
-package cherryComponent
-
-//type IClusterComponent interface {
-//    SendUserMessage(session *cherrySession.Session, route *cherryRoute.Route, msg *cherryMessage.Message)
-//    SendSysMessage(nodeId string, msg *cherryProto.Message)
-//    SendCloseSession(sessionId cherryFacade.SID)
-//    OnCloseSession(func(sid cherryFacade.SID))
-//    OnForward(func(msg *cherryProto.Message))
-//}
--- component/handler_component.go
+++
@@ -1,6 +0,0 @@
-package cherryComponent
-
-//type IHandlerComponent interface {
-//	PostEvent(event facade.IEvent)
-//	PostMessage(session *cherrySession.Session, msg *cherryMessage.Message)
-//}
