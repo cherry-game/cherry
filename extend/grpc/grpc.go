c3105624ca64742c2d1ae8e5490107789c80d118
ryansu <phantacix@qq.com>
2021-07-28 20:43:10 +0800

    clean code

--- extend/grpc/grpc.go
+++ extend/grpc/grpc.go
@@ -2,7 +2,9 @@ package cherryGRPC
 
 import (
 	"context"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
+	"time"
 )
 
 func GetIP(ctx context.Context) string {
@@ -11,4 +13,24 @@ func GetIP(ctx context.Context) string {
 		return ""
 	}
 	return p.Addr.String()
+}
+
+func BuildRpcClientConn(address string, timeout time.Duration, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	if timeout.Seconds() < 1 {
+		timeout = 2 * time.Second
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	dialConn, err := grpc.DialContext(
+		ctx,
+		address,
+		opts...,
+	)
+	cancel()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dialConn, nil
 }
