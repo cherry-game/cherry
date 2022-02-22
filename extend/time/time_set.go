9459fa9bd5f15a4b58ec4b73701f5b41c4b02c2c
ryansu <phantacix@qq.com>
2022-02-22 14:39:49 +0800

    set func return value

--- extend/time/time_set.go
+++ extend/time/time_set.go
@@ -13,43 +13,43 @@ func (c *CherryTime) SetTimezone(timezone string) error {
 }
 
 // SetYear 设置年
-func (c *CherryTime) SetYear(year int) *CherryTime {
+func (c CherryTime) SetYear(year int) CherryTime {
 	c.Time = time.Date(year, c.Time.Month(), c.Day(), c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
 // SetMonth 设置月
-func (c *CherryTime) SetMonth(month int) *CherryTime {
+func (c CherryTime) SetMonth(month int) CherryTime {
 	c.Time = time.Date(c.Year(), time.Month(month), c.Day(), c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
 // SetDay 设置日
-func (c *CherryTime) SetDay(day int) *CherryTime {
+func (c CherryTime) SetDay(day int) CherryTime {
 	c.Time = time.Date(c.Year(), c.Time.Month(), day, c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
 // SetHour 设置时
-func (c *CherryTime) SetHour(hour int) *CherryTime {
+func (c CherryTime) SetHour(hour int) CherryTime {
 	c.Time = time.Date(c.Year(), c.Time.Month(), c.Day(), hour, c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
 // SetMinute 设置分
-func (c *CherryTime) SetMinute(minute int) *CherryTime {
+func (c CherryTime) SetMinute(minute int) CherryTime {
 	c.Time = time.Date(c.Year(), c.Time.Month(), c.Day(), c.Hour(), minute, c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
 // SetSecond 设置秒
-func (c *CherryTime) SetSecond(second int) *CherryTime {
+func (c CherryTime) SetSecond(second int) CherryTime {
 	c.Time = time.Date(c.Year(), c.Time.Month(), c.Day(), c.Hour(), c.Minute(), second, c.Nanosecond(), c.Location())
 	return c
 }
 
 // SetNanoSecond 设置纳秒
-func (c *CherryTime) SetNanoSecond(nanoSecond int) *CherryTime {
+func (c CherryTime) SetNanoSecond(nanoSecond int) CherryTime {
 	c.Time = time.Date(c.Year(), c.Time.Month(), c.Day(), c.Hour(), c.Minute(), c.Second(), nanoSecond, c.Location())
 	return c
 }
