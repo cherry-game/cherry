14327d24ab531ebab7d036d8530c6ff4794f05d4
ryansu <phantacix@qq.com>
2022-02-22 11:43:38 +0800

    add logger benchmark

---
+++ logger/logger_test.go
@@ -0,0 +1,20 @@
+package cherryLogger
+
+import (
+	cherryTime "github.com/cherry-game/cherry/extend/time"
+	"testing"
+)
+
+func BenchmarkWrite(b *testing.B) {
+	config := defaultConsoleConfig()
+	config.EnableConsole = false
+	config.EnableWriteFile = true
+	config.FileLinkPath = "logs/log1.log"
+	config.FilePathFormat = "logs/log1_%Y%m%d%H%M.log"
+
+	log1 := NewConfigLogger(config)
+
+	for i := 0; i < b.N; i++ {
+		log1.Debug(cherryTime.Now().ToDateTimeFormat())
+	}
+}
