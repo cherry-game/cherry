69a95e20a38a6159a686ac8bdeb238d3f9a31fe9
ryansu <phantacix@qq.com>
2022-03-30 09:33:05 +0800

    add: 客户端连接器增加心跳日志打印

--- net/client/client.go
+++ net/client/client.go
@@ -317,7 +317,7 @@ func (p *Client) handleData() {
 				if err := p.SendRaw(cherryPacket.Heartbeat, []byte{}); err != nil {
 					cherryLogger.Warnf("[%s] packet encode error. %s", p.TagName, err.Error())
 				} else {
-					cherryLogger.Debug("[%s] heart beat.", p.TagName)
+					cherryLogger.Debugf("[%s] heart beat.", p.TagName)
 				}
 			}
 		case <-p.closeChan:
