4846ff711def6faab6d845f768d21f503e4e5319
ryansu <phantacix@qq.com>
2022-10-13 11:06:21 +0800

    update: clean warn

--- extend/time/time_offset_test.go
+++ extend/time/time_offset_test.go
@@ -1,24 +1,23 @@
 package cherryTime
 
 import (
-	cherryLogger "github.com/cherry-game/cherry/logger"
 	"testing"
 	"time"
 )
 
 func TestGlobalOffset(t *testing.T) {
 	now := Now()
-	cherryLogger.Info(now.ToMillisecond(), " - ", now.ToDateTimeFormat())
+	t.Log(now.ToMillisecond(), " - ", now.ToDateTimeFormat())
 
 	SubOffsetTime(SecondsPerDay * time.Second)
 	nowOffset := Now()
-	cherryLogger.Info(nowOffset.ToMillisecond(), " - ", nowOffset.ToDateTimeFormat())
+	t.Log(nowOffset.ToMillisecond(), " - ", nowOffset.ToDateTimeFormat())
 
 	nowBackup := nowOffset
 	nowOffset.SubDay()
-	cherryLogger.Info(nowOffset.ToMillisecond(), " - ", nowOffset.ToDateTimeFormat())
-	cherryLogger.Info(nowBackup.ToMillisecond(), " - ", nowBackup.ToDateTimeFormat())
+	t.Log(nowOffset.ToMillisecond(), " - ", nowOffset.ToDateTimeFormat())
+	t.Log(nowBackup.ToMillisecond(), " - ", nowBackup.ToDateTimeFormat())
 
 	nowOffset.AddDays(1)
-	cherryLogger.Info(nowOffset.ToMillisecond(), " - ", nowOffset.ToDateTimeFormat())
+	t.Log(nowOffset.ToMillisecond(), " - ", nowOffset.ToDateTimeFormat())
 }
