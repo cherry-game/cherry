f890fb3bce2014cb699a52b35d949405b4ed7fd8
phantaci <phantaci@123.com>
2026-04-17 20:36:35 +0800

    feat(tag): 更新标签脚本以显示当前版本并支持默认版本输入

--- tag.sh
+++ tag.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+CURRENT_VERSION=$(sed -n 's/.*version = "\([^"]*\)".*/\1/p' const/const.go)
+echo "[TAG] current version: ${CURRENT_VERSION}"
 echo "[TAG] ------------------------------------------------------"
 
+PREFIX=$(echo "$CURRENT_VERSION" | cut -d'.' -f1,2)
+LAST=$(echo "$CURRENT_VERSION" | cut -d'.' -f3)
+DEFAULT_VERSION="${PREFIX}.$((LAST + 1))"
+
 # shellcheck disable=SC2162
-read -p "[TAG] enter new tag(eg. 1.3.14):" number
+read -p "[TAG] enter new tag(default: ${DEFAULT_VERSION}):" number
+number=${number:-$DEFAULT_VERSION}
 
 echo ""
-
 
 echo "[TAG ${number}] cherry"
 git tag -a "v${number}" -m "auto tag"
--- ver.sh
+++ ver.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+CURRENT_VERSION=$(sed -n 's/.*version = "\([^"]*\)".*/\1/p' const/const.go)
+echo "[VER] current version: ${CURRENT_VERSION}"
 echo "[VER] ------------------------------------------------------"
 
-# shellcheck disable=SC2162
-read -p "[VER] enter new version(eg. 1.3.14):" number
+PREFIX=$(echo "$CURRENT_VERSION" | cut -d'.' -f1,2)
+LAST=$(echo "$CURRENT_VERSION" | cut -d'.' -f3)
+DEFAULT_VERSION="${PREFIX}.$((LAST + 1))"
 
-if [[ "$OSTYPE" == "linux"* ]] || [[ "$OSTYPE" == "msys"* ]] ; then
-    echo "[VER] use sed"
-    sed -i 's/version = "[0-9.]*"/version = "'"${number}"'"/' const/const.go
+# shellcheck disable=SC2162
+read -p "[VER] enter new version(default: ${DEFAULT_VERSION}):" number
+number=${number:-$DEFAULT_VERSION}
+
+if [[ "$OSTYPE" == "linux"* ]] || [[ "$OSTYPE" == "msys"* ]]; then
+	echo "[VER] use sed"
+	sed -i 's/version = "[0-9.]*"/version = "'"${number}"'"/' const/const.go
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "[VER] use gsed"
-    gsed -i 's/version = "[0-9.]*"/version = "'"${number}"'"/' const/const.go
+	echo "[VER] use gsed"
+	gsed -i 's/version = "[0-9.]*"/version = "'"${number}"'"/' const/const.go
 fi
