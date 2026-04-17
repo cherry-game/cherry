2f1ae3800a4d670019c08e6b5024f7a3600fba0d
phantaci <phantaci@123.com>
2026-04-17 20:37:14 +0800

    chore: 修改tag.sh的日志输出

--- tag.sh
+++ tag.sh
@@ -4,17 +4,13 @@ CURRENT_VERSION=$(sed -n 's/.*version = "\([^"]*\)".*/\1/p' const/const.go)
 echo "[TAG] current version: ${CURRENT_VERSION}"
 echo "[TAG] ------------------------------------------------------"
 
-PREFIX=$(echo "$CURRENT_VERSION" | cut -d'.' -f1,2)
-LAST=$(echo "$CURRENT_VERSION" | cut -d'.' -f3)
-DEFAULT_VERSION="${PREFIX}.$((LAST + 1))"
-
 # shellcheck disable=SC2162
-read -p "[TAG] enter new tag(default: ${DEFAULT_VERSION}):" number
-number=${number:-$DEFAULT_VERSION}
+read -p "[TAG] enter new tag(current: ${CURRENT_VERSION}):" number
+number=${number:-$CURRENT_VERSION}
 
 echo ""
 
-echo "[TAG ${number}] cherry"
+echo "[TAG] create ${number}"
 git tag -a "v${number}" -m "auto tag"
 
 echo "[TAG] ------------------------------------------------------"
