5da01f0ee46555da32d4e4d6f228be6da97cae14
phantaci <phantaci@123.com>
2026-05-09 15:08:42 +0800

    feat: 添加更新设置功能，支持批量更新和单个更新

--- net/proto/proto.go
+++ net/proto/proto.go
@@ -3,3 +3,17 @@ package cherryProto
 func (x *Member) IsTimeout(nowMills int64) bool {
 	return x.LastAt+x.HeartbeatTimeout < nowMills
 }
+
+func (x *Member) UpdateSettings(settings map[string]string) {
+	if settings == nil {
+		return
+	}
+
+	for k, v := range settings {
+		x.Settings[k] = v
+	}
+}
+
+func (x *Member) UpdateSetting(key, value string) {
+	x.Settings[key] = value
+}
