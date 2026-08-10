ad3833859183f8988531d29833bc800851abd092
zhangyadong <zhangyadong@astrapolus.com>
2026-08-10 10:01:55 +0800

    offsetTime 修改成 atomic.Int64类型,方便逻辑层 动态改变时间偏移量

--- extend/time/time.go
+++ extend/time/time.go
@@ -55,7 +55,7 @@ func NewTime(tt time.Time, setGlobal bool) CherryTime {
 	ct := CherryTime{}
 
 	if setGlobal {
-		ct.Time = tt.In(offsetLocation).Add(offsetTime)
+		ct.Time = tt.In(offsetLocation).Add(time.Duration(offsetTime.Load()))
 	} else {
 		ct.Time = tt
 	}
